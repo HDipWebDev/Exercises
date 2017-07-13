4c33e746b986412048bee26f9f033ec726bf8b1a
Paul Vesey <pvesey@eircom.net>
2017-07-13 21:46:32 +0100

    Flexbox Started
    Flexbox section added

--- JavaScript/bsEx05/js/IndexedDB.js
+++ JavaScript/bsEx05/js/IndexedDB.js
@@ -7,19 +7,23 @@ var db;
 function initJS(){
 
   setupIndexedDB();
-
   reflectCode();
 }
 
 
-
 function setupIndexedDB(){
   
+  const todoTaskList = [
+    { tasks: "Learn Doxygen", complete: 'in progress', date: "bill@company.com" },
+    { tasks: "Unit Testing", complete: 'no', date: "bill@company.com" },  
+    { tasks: "Fix Broken Code", complete: 'yes', date: "donna@home.org" }
+  ];
+
   if (!window.indexedDB) {
       window.alert("IndexedDB support not implemented in your browser..");
   }
 
-  var request = window.indexedDB.open("HDipTestDatabase", 2);
+  var request = window.indexedDB.open("HDipTestDatabase", 1);
 
   request.onerror = function (event) {
     console.log("IndexedDB Error :", event.target.errorCode)
@@ -33,12 +37,20 @@ function setupIndexedDB(){
 
   request.onupgradeneeded = function (event) {
     console.log('Upgrading Database');
+    var db = event.target.result;
+    var objectStore = db.createObjectStore('todoList', {keyPath: 'date'});
+    objectStore.createIndex('tasks', 'tasks', { unique: false });
+    objectStore.createIndex('complete', 'complete', { unique: false });
 
-    var objectStore = db.createObjectStore('movies', {keyPath: 'IMDBKey'});
-
-
+    objectStore.transaction.oncomplete = function(event) {
+      // Store values in the newly created objectStore.
+      var objectStore = db.transaction('tasks', 'readwrite').objectStore('todoList');
+      for (var i in tasks) {
+        objectStore.add(todoTaskList[i]);
+        console.log(todoTaskList[i])
+      }
+    };
   }
-
 }
 
 
